19e2ed3b00a8e94a3bd0bd6a7fbb68b0f94afa31
Vitor Varjão <vitornvj@gmail.com>
2022-01-21 21:20:13 -0300

    test: ensure CreateUserService returns false if findUserRepository returns a user

---
+++ tests/data/services/CreateUserService.spec.ts
@@ -0,0 +1,33 @@
+import { CreateUserService } from '@/app/services/users/CreateUserService';
+import { mockCreateAccountParams } from '@/tests/domain/mocks';
+
+import { CreateUserRepositorySpy, FindUserRepositorySpy } from '../mocks';
+
+const makeSut = () => {
+  const createUserRepositorySpy = new CreateUserRepositorySpy();
+  const findUserRepositorySpy = new FindUserRepositorySpy();
+  const sut = new CreateUserService(
+    findUserRepositorySpy,
+    createUserRepositorySpy,
+  );
+  return {
+    sut,
+    createUserRepositorySpy,
+    findUserRepositorySpy,
+  };
+};
+describe('CreateUser Service', () => {
+  it('Should return false if findUserRepositorySpy returns a user', async () => {
+    const { sut, findUserRepositorySpy } = makeSut();
+    const result = await sut.create(mockCreateAccountParams());
+    expect(result).toBe(false);
+  });
+  it('Should throw if findUserRepository throws', async () => {
+    const { sut, findUserRepositorySpy } = makeSut();
+    jest
+      .spyOn(findUserRepositorySpy, 'findByEmail')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const error = sut.create(mockCreateAccountParams());
+    await expect(error).rejects.toThrow();
+  });
+});
