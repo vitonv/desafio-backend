b3950488d7b0b88ab1d6040b120a9dc4496a43bc
Vitor Varjão <vitornvj@gmail.com>
2022-01-22 00:43:58 -0300

    test: ensure FindUserByTokenService returns null if FindUserByTokenRepository do not return a user

---
+++ tests/app/services/users/FindByToken.spec.ts
@@ -0,0 +1,22 @@
+import { FindUserByTokenService } from '@/app/services/users';
+
+import { FindUserByTokenRepositorySpy } from '../../mocks';
+
+const makeSut = () => {
+  const findUserByTokenRepositorySpy = new FindUserByTokenRepositorySpy();
+  const sut = new FindUserByTokenService(findUserByTokenRepositorySpy);
+  return {
+    sut,
+    findUserByTokenRepositorySpy,
+  };
+};
+describe('FindByToken Service', () => {
+  it('Should return null if findUserByTokenRepository do not return an user', async () => {
+    const { sut, findUserByTokenRepositorySpy } = makeSut();
+    jest
+      .spyOn(findUserByTokenRepositorySpy, 'findByToken')
+      .mockReturnValueOnce(Promise.resolve(null));
+    const result = await sut.findByToken('any_token');
+    expect(result).toBeNull();
+  });
+});
