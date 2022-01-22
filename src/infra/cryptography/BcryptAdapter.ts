70251f80bee056de4a77aec23bb9590c09e85a3d
Vitor Varjão <vitornvj@gmail.com>
2022-01-21 23:55:58 -0300

    feat: ensure BcryptAdapter returns true if compare succeeds

--- src/infra/cryptography/BcryptAdapter.ts
+++ src/infra/cryptography/BcryptAdapter.ts
@@ -6,7 +6,7 @@ import { Hasher } from '@/app/protocols/cryptography/Hasher';
 export class BcryptAdapter implements Hasher, HashComparer {
   constructor(private readonly salt: number) {}
   async compare(plaintext: string, digest: string): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    return bcrypt.compare(plaintext, digest);
   }
   async hash(plaintext: string): Promise<string> {
     return bcrypt.hash(plaintext, this.salt);
