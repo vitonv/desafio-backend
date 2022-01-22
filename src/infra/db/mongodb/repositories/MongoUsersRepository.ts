ca7e19a86f42ce7c92279cd683d6f259d40a4ca3
Vitor Varjão <vitornvj@gmail.com>
2022-01-21 23:35:33 -0300

    feat: ensure MongoUsersRepository update token field off an existant user

--- src/infra/db/mongodb/repositories/MongoUsersRepository.ts
+++ src/infra/db/mongodb/repositories/MongoUsersRepository.ts
@@ -26,33 +26,22 @@ export class MongoUsersRepository
     return !!insertedId;
   }
   async updateAccessToken(id: string, token: string): Promise<void> {
-    throw new Error('Method not implemented.');
+    await this.repository.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          token,
+        },
+      },
+    );
   }
   async findByEmail(email: string): Promise<User> {
-    throw new Error('Method not implemented.');
+    const findUser = await this.repository.findOne({ email });
+    if (findUser) {
+      return MongoHelper.map(findUser);
+    }
+    return null;
   }
 }
-
-// async create(user: CreateAccount.Params): Promise<boolean> {
-//   const created = await this.repository.insertOne(user);
-//   return !!created.insertedId;
-// }
-// async findByEmail(email: string): Promise<User> {
-//   const findUser = await this.repository.findOne({ email });
-//   if (findUser) {
-//     return MongoHelper.map(findUser);
-//   }
-//   return null;
-// }
-// async updateToken(id: string, token: string): Promise<void> {
-//   await this.repository.updateOne(
-//     {
-//       _id: id,
-//     },
-//     {
-//       $set: {
-//         accessToken: token,
-//       },
-//     },
-//   );
-// }
