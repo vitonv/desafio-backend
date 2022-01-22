f312587ee57924397e43f544ed0c0ec200a73e54
Vitor Varjão <vitornvj@gmail.com>
2022-01-21 22:25:12 -0300

    feat: ensure AuthenticateUserService calls encrypter with correct id

--- src/app/services/users/authenticate/index.ts
+++ src/app/services/users/authenticate/index.ts
@@ -1,4 +1,5 @@
 import { HashComparer } from '@/app/protocols/cryptography';
+import { Encrypter } from '@/app/protocols/cryptography/Encrypter';
 import { FindUserRepository } from '@/app/protocols/db/users/FindUsersRepository';
 import { Authentication } from '@/domain/useCases/users/Authentication';
 import { FindUserRepositorySpy } from '@/tests/app/mocks';
@@ -7,14 +8,18 @@ export class AuthenticateUserService implements Authentication {
   constructor(
     private readonly findUserRepository: FindUserRepository,
     private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
   ) {}
 
   async auth(params: Authentication.Params): Promise<Authentication.Result> {
     const user = await this.findUserRepository.findByEmail(params.email);
-    const passwordMatch = await this.hashComparer.compare(
-      params.password,
-      user.password,
-    );
+    if (user) {
+      const passwordMatch = await this.hashComparer.compare(
+        params.password,
+        user.password,
+      );
+      const token = await this.encrypter.encrypt(user.id);
+    }
 
     return null;
   }
--- tests/app/mocks/MockCryptography.ts
+++ tests/app/mocks/MockCryptography.ts
@@ -1,7 +1,13 @@
-import { HashComparer } from '@/app/protocols/cryptography';
+import { Encrypter, HashComparer } from '@/app/protocols/cryptography';
 
 export class HashComparerSpy implements HashComparer {
   async compare(plaintext: string, digest: string): Promise<boolean> {
     return Promise.resolve(true);
+  }
+}
+
+export class EncrypterSpy implements Encrypter {
+  async encrypt(text: string): Promise<string> {
+    return Promise.resolve('any_token');
   }
 }
