f7e7c1e377768e5c0c6342f1fc08c8bee071a5bb
Vitor Varjão <vitornvj@gmail.com>
2022-01-22 00:59:47 -0300

    test: ensure FindByTokenService returns null if decrypter throws

--- tests/app/services/users/FindByToken.spec.ts
+++ tests/app/services/users/FindByToken.spec.ts
@@ -1,16 +1,30 @@
 import { FindUserByTokenService } from '@/app/services/users';
 
 import { FindUserByTokenRepositorySpy } from '../../mocks';
+import { DecrypterSpy } from '../../mocks/MockCryptography';
 
 const makeSut = () => {
+  const decrypterSpy = new DecrypterSpy();
   const findUserByTokenRepositorySpy = new FindUserByTokenRepositorySpy();
-  const sut = new FindUserByTokenService(findUserByTokenRepositorySpy);
+  const sut = new FindUserByTokenService(
+    decrypterSpy,
+    findUserByTokenRepositorySpy,
+  );
   return {
     sut,
     findUserByTokenRepositorySpy,
+    decrypterSpy,
   };
 };
 describe('FindByToken Service', () => {
+  it('Should return null if decrypter throws', async () => {
+    const { sut, decrypterSpy } = makeSut();
+    jest
+      .spyOn(decrypterSpy, 'decrypt')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const result = await sut.findByToken('any_token');
+    expect(result).toBeNull();
+  });
   it('Should return null if findUserByTokenRepository do not return an user', async () => {
     const { sut, findUserByTokenRepositorySpy } = makeSut();
     jest
